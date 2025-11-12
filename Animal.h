020958e70f67fcd6cb8db0708d8972a8da880285
16006684MohamedAbdrabo <niufdb@wireconnected.com>
2025-11-12 14:46:49 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -4,8 +4,31 @@
 #include <string>
 using namespace std;
 
-//define animal class here 
+Class Animal {
+    private:
+    String name;
+    int age;
+    bool isHungry;
+    
+Animal(String name,int age, bool isHungry) {
+    name = String n;
+    int age = age;
+    bool isHungry = hungry;
+}
 
+void display() {
+    cout << "Name:" << n << endl;
+    cout << "Age: " << age << endl;
+    cout << "Hunger status: " << hungry << endl;
+    
+}
+
+void feed() {
+    if (hungry = false) {
+    cout << "The animal is not hungry";
+    } else { cout << "The animal is hungry";
+    
+    }}
 
 
 #endif
