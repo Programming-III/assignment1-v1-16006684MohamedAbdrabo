6250afa5eb74e980349596bf870bf1c276574300
16006684MohamedAbdrabo <niufdb@wireconnected.com>
2025-11-12 14:47:42 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -3,8 +3,30 @@
 
 #include "Animal.h"
 
-//define enclosure class here 
-
+Class Enclosure {
+    
+    private: 
+    Animal*arr[];
+    int capacity;
+    int currentCount;
+    
+    Enclosure(Animal*arr[], int capacity, int currentCount) {
+    
+    int capacity = capacity;
+    int currentCount = count;}
+    
+    
+    
+    void addAnimal(Animal*a) {
+    
+    Animal*arr[] += Animal*a;
+    }
+    
+    void displayAnimals() {
+    cout << "Animals in this enclosure: " << Animal::display() << endl;
+    }
+    
+}
 
 
 
